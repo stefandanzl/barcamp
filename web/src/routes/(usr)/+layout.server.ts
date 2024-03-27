a66538f8b0d44c01be2a7226bf5813e7a1bc7767
Stefan Danzl <stefan.danzl@live.de>
2024-03-27 09:48:29 +0100

    some data stuff

--- web/src/routes/(usr)/+layout.server.ts
+++ web/src/routes/(usr)/+layout.server.ts
@@ -2,6 +2,12 @@ import { redirect } from '@sveltejs/kit';
 
 export function load({ locals }) {
 	if (!locals.user) redirect(303, '/login');
+
+	
+	return {
+		email : locals.user.email,
+		id: locals.user.id
+	}
 }
 
 
--- web/src/routes/(usr)/+layout.svelte
+++ web/src/routes/(usr)/+layout.svelte
@@ -4,6 +4,7 @@
 	import { Tabs } from 'stwui';
 	import { Toggle } from 'stwui';
 	import { page } from '$app/stores';  
+	export let data;
 	
 
     const home =  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>home-account</title><path d="M12,3L2,12H5V20H19V12H22L12,3M12,8.75A2.25,2.25 0 0,1 14.25,11A2.25,2.25 0 0,1 12,13.25A2.25,2.25 0 0,1 9.75,11A2.25,2.25 0 0,1 12,8.75M12,15C13.5,15 16.5,15.75 16.5,17.25V18H7.5V17.25C7.5,15.75 10.5,15 12,15Z" /></svg>';
@@ -102,7 +103,7 @@ let visible = false;
 			>
         <slot />
 
-		
+		<div>{JSON.stringify(data.user?.id,null,4)}</div>
 			</Layout.Content.Body
 		>
 	</Layout.Content>
